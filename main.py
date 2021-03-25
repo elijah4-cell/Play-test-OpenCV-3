0d7dcda0314b67ceb00ce41e85a6e09afad95339
Elijah Salt <elijah.salt@s.elim.school.nz>
2021-03-25 20:34:52 +0000

    Add 9 more images and comment out grayscale output

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import cv2
 import numpy as np
+#import os
+
+#https://www.tutorialspoint.com/python/os_listdir.htm
+# open file
+#path = "tennis balls"
+#dirs = os.listdir( path )
+
+# print files
+#for file in dirs:
+# print (file)
+
+
 
 # read in image file
-img = cv2.imread("tennis ball 2.jpeg")
+img = cv2.imread("tennis balls/tennis ball 2.jpeg")
 
 #blur output image
 #img = cv2.medianBlur(gray_output ,5)
 
 #https://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/
-
 # https://www.rapidtables.com/web/color/RGB_Color.html
-
 # define the boundaries
 lower_range = np.array([0, 100, 100], dtype="uint8")
 upper_range = np.array([200, 255, 255], dtype="uint8")
@@ -23,21 +33,23 @@ mask = cv2.inRange(img, lower_range, upper_range)
 output = cv2.bitwise_and(img, img, mask=mask)
 
 #turn output/tennis ball gray
-gray_output = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+#gray_output = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
 #blur output image
-gray_output = cv2.medianBlur(gray_output, 5)
+#gray_output = cv2.medianBlur(gray_output, 5)
 
 #https://docs.opencv.org/master/da/d53/tutorial_py_houghcircles.html
 #https://www.youtube.com/watch?v=dp1r9oT_h9k
 #parametres of circle
 circles = cv2.HoughCircles(gray_output,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+print (circles)
 
 #turn x and y coordinates into intergers
 detected_circles = np.uint16(np.around(circles))
+print (detected_circles)
 for (x, y, r) in detected_circles[0, :]:
-    cv2.circle(gray_output, (x, y), r, (100, 100, 120), 3)
-    cv2.circle(gray_output, (x, y), 2, (0, 0, 255), 3)
+    cv2.circle(img, (x, y), r, (100, 100, 120), 3)
+    cv2.circle(img, (x, y), 2, (0, 0, 255), 3)
 
 #print coordinates of centre and radius
 print (x, y, r)
@@ -46,4 +58,4 @@ print (x, y, r)
 cv2.imwrite("c_output.png", img)
 
 # output file
-cv2.imwrite("output.png", gray_output)
+#cv2.imwrite("output.png", gray_output)
